1997e96d41aa2232358e57d552c321eafcc993a3
TakumuIseki <t-iseki@kawahara.ac.jp>
2025-07-26 05:09:06 +0900

    コントローラーの接続確認処理と準備完了のON/OFF

---
+++ Assets/Game/Scripts/OutGame/ConnectController.cs
@@ -0,0 +1,87 @@
+﻿///
+/// コントローラー接続処理
+///
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+
+public class ConnectController : MonoBehaviour
+{
+    [Header("プレイヤー番号"), SerializeField]
+    private int playerNum_ = 0;
+
+    [Header("コントローラー接続画像"),SerializeField]
+    private Sprite ConectControllerSprite;
+
+    [Header("プレイヤー参加画像"), SerializeField]
+    private Sprite RegisterPlayerSprite;
+
+    [Header("プレイヤー登録画像"), SerializeField]
+    private Image RegisterPlayerImage;
+
+    /// <summary>
+    /// コントローラー管理
+    /// </summary>
+    private ControllerManager controllerManager_;
+
+    /// <summary>
+    /// オブジェクトがアクティブになったときに1度だけ呼び出される
+    /// </summary>
+    private void Start()
+    {
+        controllerManager_ = FindObjectOfType<ControllerManager>();
+
+        // コントローラーの接続状況を監視
+        controllerManager_.ConnectedControllers.Subscribe(count =>
+            {
+                if (count >= playerNum_)
+                {
+                    RegisterPlayerImage.sprite = RegisterPlayerSprite;
+                    GoReadyTask().Forget();
+                }
+                else
+                {
+                    RegisterPlayerImage.sprite = ConectControllerSprite;
+                    ChangeColor(Color.white);
+                }
+            }
+        );
+    }
+
+    /// <summary>
+    /// プレイヤー準備完了までのタスク
+    /// </summary>
+    private async UniTask GoReadyTask()
+    {
+        // Aボタンが押されるまで待機
+        await UniTask.WaitUntil(() => Input.GetKeyDown(KeyCode.JoystickButton0));
+
+        // プレイヤー準備完了状態
+        ChangeColor(Color.yellow);
+
+        BackReadyTask().Forget();
+    }
+
+    /// <summary>
+    /// プレイヤー準備中までのタスク
+    /// </summary>
+    private async UniTask BackReadyTask()
+    {
+        // Bボタンが押されるまで待機
+        await UniTask.WaitUntil(() => Input.GetKeyDown(KeyCode.JoystickButton1));
+
+        // プレイヤー準備中状態
+        ChangeColor(Color.white);
+
+        GoReadyTask().Forget();
+    }
+
+    /// <summary>
+    /// 色変更
+    /// </summary>
+    private void ChangeColor(Color color)
+    {
+        RegisterPlayerImage.color = color;
+    }
+}
---
+++ Assets/Game/Scripts/Utility/ControllerManager.cs
@@ -0,0 +1,45 @@
+﻿///
+/// コントローラー管理
+///
+using R3;
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    /// <summary>
+    /// 接続されているコントローラー数プロパティ
+    /// </summary>
+    private ReactiveProperty<int> connectedControllerNum_ = new ReactiveProperty<int>();
+
+    /// <summary>
+    /// 接続されているコントローラーのObserver
+    /// </summary>
+    public Observable<int> ConnectedControllers => connectedControllerNum_.AsObservable();
+
+    /// <summary>
+    /// オブジェクトがアクティブになったときに1度だけ呼び出される
+    /// </summary>
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    private void Update()
+    {
+        // コントローラーの接続状態を更新
+
+        var names = Input.GetJoystickNames();
+        var newJoysticks = names.Where(name => !string.IsNullOrEmpty(name)).ToList();
+
+        // 接続コントローラー数が変わった場合のみ更新通知
+        if (connectedControllerNum_.Value != newJoysticks.Count)
+        {
+            connectedControllerNum_.Value = newJoysticks.Count;
+        }
+    }
+}
