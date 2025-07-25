3914232365283ea35697b61be4335e26d50db5b0
TakumuIseki <t-iseki@kawahara.ac.jp>
2025-07-25 12:31:50 +0900

    タイトル→プレイヤー登録へ遷移

---
+++ Assets/Game/Scripts/OutGame/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿///
+/// タイトルマネージャー
+///
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            // プレイヤー登録シーンへ遷移
+            SceneManager.LoadScene(SceneNameConst.RegisterPlayerSceneName);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Utility/SceneNameConst.cs
@@ -0,0 +1,26 @@
+﻿///
+/// シーン名定数
+///
+
+public static class SceneNameConst
+{
+    /// <summary>
+    /// タイトルシーン名
+    /// </summary>
+    public static readonly string TitleSceneName = "TitleScene";
+
+    /// <summary>
+    /// プレイヤー登録シーン名
+    /// </summary>
+    public static readonly string RegisterPlayerSceneName = "RegisterPlayerScene";
+
+    /// <summary>
+    /// ゲームシーン名
+    /// </summary>
+    public static readonly string GameSceneName = "GameScene";
+
+    /// <summary>
+    /// ランキングシーン名
+    /// </summary>
+    public static readonly string RankingSceneName = "RankingScene";
+}
