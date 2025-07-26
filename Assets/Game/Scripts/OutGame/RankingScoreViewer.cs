2934b67bb1dc51fb30e9c1a9505e3619e3b30b24
Sugahara <tatuyukisugahara@gmail.com>
2025-07-26 23:26:31 +0900

    ランキングシーン対応
    セーブデータから取得したランキングデータをもとに表示。
    高さ順に並べ替えをして使用しています。
    ※SaveSystemが常駐シーンに生成されている必要がありますが、まだ常駐シーンがないため確認は別途対応が必要です。

---
+++ Assets/Game/Scripts/OutGame/RankingScoreViewer.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 表示するランキング順位
+/// </summary>
+enum RankingType
+{
+    First,
+    Second,
+    Third,
+}
+
+public class RankingScoreViewer : MonoBehaviour
+{
+    // 表示するランキングの順位
+    [SerializeField]
+    private RankingType rankingType_ = RankingType.First;
+
+    
+    
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+        // ランキングデータの取得
+        var rankingDatas = SaveData.Get().GetRankingDatas();
+        // 指定した順位がランキングデータにない場合は非表示にしておく
+        if (rankingDatas.Count < (int)rankingType_)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        // ランキングデータのソート
+        // 高いスコア順にソート
+        rankingDatas.Sort((a, b) => b.height.CompareTo(a.height));
+        // ランキングデータをテキストに反映 
+        var children = GetComponentsInChildren<TextMeshProUGUI>();
+        Debug.Log("Children count: " + children.Length);
+        foreach (var child in children)
+        {
+            RankingData rankingData = rankingDatas[(int)rankingType_];
+            if (child.gameObject.name == "Name")
+            {
+                SetName(child, rankingData.name);
+            }
+            else if (child.gameObject.name == "Score")
+            {
+                SetScore(child, rankingData.height);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// ユーザー名設定
+    /// </summary>
+    private void SetName(TextMeshProUGUI textMeshPro, string name)
+    {
+        if (textMeshPro == null)
+        {
+            return;
+        }
+        textMeshPro.text = name;
+    }
+
+
+    /// <summary>
+    /// スコア設定
+    /// </summary>
+    private void SetScore(TextMeshProUGUI textMeshPro, uint score)
+    {
+        if (textMeshPro == null)
+        {
+            return;
+        }
+        textMeshPro.text = score.ToString() + "m"; // スコアの単位をメートルに設定
+    }
+}
