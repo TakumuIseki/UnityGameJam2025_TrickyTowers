6489aa96537753f5e0fb61aa58efb0f4aa309051
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-07-28 17:57:05 +0900

    <TetriminoController.cs>追加

---
+++ Assets/TetriminoController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TetrominoController : MonoBehaviour
+{
+    public PlayerUnit owner;
+    private Rigidbody2D rb;
+    private bool hasLanded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!hasLanded && collision.gameObject.CompareTag("Ground"))
+        {
+            hasLanded = true;
+            rb.bodyType = RigidbodyType2D.Static;
+
+            // 落下完了を通知
+            owner.OnTetrominoLanded();
+        }
+    }
+}
