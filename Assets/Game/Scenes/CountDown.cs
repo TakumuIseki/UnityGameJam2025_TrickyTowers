c13affd04de788a61bd9211d2ea7147c03737c84
Hitota Ryuki <hogehoge0125@gmail.com>
2025-07-28 15:32:42 +0900

    ゲーム開始前のカウントダウンの実装

---
+++ Assets/Game/Scenes/CountDown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    public TextMeshProUGUI countText;
+    float countDown = 4f;
+    int count = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowText();
+    }
+
+    void ShowText() 
+    {
+        if (countDown >= 1)
+        {
+            countDown -= Time.deltaTime;
+            count = (int)countDown;
+            countText.text = count.ToString();
+        }
+        if (countDown <= 1)
+        {
+            countText.text = "GO!";
+
+        }
+    }
+
+    void StartGame() {
+        //カウントダウンが終わったらゲームを開始する。
+    }
+}
