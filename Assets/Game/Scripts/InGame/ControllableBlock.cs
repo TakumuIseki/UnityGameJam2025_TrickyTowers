428f63e4927d0179717ee2898df1812996ff80fc
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-01 17:01:21 +0900

    ControllableBlock.csを編集しました。マジックナンバーの定数化、移動処理の関数化、ValidMovement()の削除、FixBlockの削除。

--- Assets/Game/Scripts/InGame/ControllableBlock.cs
+++ Assets/Game/Scripts/InGame/ControllableBlock.cs
@@ -1,19 +1,18 @@
-﻿using UnityEngine;
+﻿///
+/// テトリミノを制御するスクリプト。
+///
+using System.Runtime.CompilerServices;
+using UnityEngine;
 
 public class ControllableBlock : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private bool isFixed = false;
 
-    [Header("落下速度")]
-    public static readonly float normalFallSpeed = 50f;  // 通常時の落下速度（units/sec）
-    public static readonly float fastFallSpeed = 100f;    // 加速時の落下速度
-
-    // ステージの大きさ
-    public static readonly int width = 400;
-    public static readonly int height = 800;
-
-
+    private static readonly float NORMAL_FALL_SPEED = 50f;                  // 通常時の落下速度（units/sec）
+    private static readonly float FAST_FALL_SPEED = 100f;                   // 加速時の落下速度
+    private static readonly float MOVE_DISTANCE_PER_KEY = 10f;              // 左右移動の幅
+    private static readonly Vector3 ROTATION_ANGLE = new Vector3(0, 0, 90); // 回転角度
+    private static readonly float GRAVITY = 30f;                            // 重力
 
     void Start()
     {
@@ -24,73 +23,35 @@ public class ControllableBlock : MonoBehaviour
 
     void Update()
     {
-        if (isFixed) return;
-
-        float fallSpeed = Input.GetKey(KeyCode.DownArrow) ? fastFallSpeed : normalFallSpeed;
+        float fallSpeed = Input.GetKey(KeyCode.DownArrow) ? FAST_FALL_SPEED : NORMAL_FALL_SPEED;
 
         // 時間によって位置を下げる（transformを直接動かす）
         transform.position += Vector3.down * fallSpeed * Time.deltaTime;
 
-        // 左右移動
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector3.left * 10, Space.World);
+        // 左に移動
+        Move(Input.GetKeyDown(KeyCode.LeftArrow), Vector3.left);
 
-            //// 今回追加
-            //if (!ValidMovement())
-            //{
-            //    transform.position -= new Vector3(-10, 0, 0);
-            //}
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * 10, Space.World);
-
-            //if (!ValidMovement())
-            //{
-            //    transform.position -= new Vector3(10, 0, 0);
-            //}
-        }
-
+        // 右に移動
+        Move(Input.GetKeyDown(KeyCode.RightArrow), Vector3.right);
 
         // 回転
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.Rotate(0, 0, 90);
+            transform.Rotate(ROTATION_ANGLE);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void Move(bool getKeyDown, Vector3 direction)
     {
-        if (!isFixed)
+        if (getKeyDown)
         {
-            FixBlock();
+            transform.Translate(direction * MOVE_DISTANCE_PER_KEY, Space.World);
         }
     }
 
-    void FixBlock()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        rb.gravityScale = 1f;
-
-        isFixed = true;
-
+        rb.gravityScale = GRAVITY;
         this.enabled = false;
-    }
-
-    bool ValidMovement()
-    {
-        foreach (Transform children in transform)
-        {
-            int roundX = Mathf.RoundToInt(children.transform.position.x);
-            int roundY = Mathf.RoundToInt(children.transform.position.y);
-
-            // minoがステージよりはみ出さないように制御
-            if (roundX < 0 || roundX >= width || roundY < 0 || roundY >= height)
-            {
-                return false;
-            }
-        }
-        return true;
     }
 }
