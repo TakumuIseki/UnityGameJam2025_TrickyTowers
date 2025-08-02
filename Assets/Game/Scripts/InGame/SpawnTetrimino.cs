533969020a0cbf5f82928cf9f283336d8c9d91b7
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-02 14:37:21 +0900

    <SpawnTetrimino.cs>一度だけテトリミノが出現する処理を作成し、動作確認を行いました。問題なく動作したため、繰り返し実行できるような処理に修正していきます。

--- Assets/Game/Scripts/InGame/SpawnTetrimino.cs
+++ Assets/Game/Scripts/InGame/SpawnTetrimino.cs
@@ -1,18 +1,22 @@
+﻿/// <summary>
+/// テトリミノをスポーンさせるスクリプト。
+/// </summary>
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnTetrimino : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] Tetriminos; // テトリミノを格納する配列。
+
     void Start()
     {
-        
+        // Tetriminosに格納されたオブジェクトの中からランダムで1つスポーンさせます。
+        Instantiate(Tetriminos[Random.Range(0, Tetriminos.Length)], transform.position, Quaternion.identity);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
