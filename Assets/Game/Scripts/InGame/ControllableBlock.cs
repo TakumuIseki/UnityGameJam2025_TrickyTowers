95a47f7f5417600c5bd4cc19fea53ec15f024a84
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2025-08-02 11:59:34 +0900

    テトリミノの作成と組み込み (#9)

---
+++ Assets/Game/Scripts/InGame/ControllableBlock.cs
@@ -0,0 +1,67 @@
+﻿///
+/// テトリミノを制御するスクリプト。
+///
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ControllableBlock : MonoBehaviour
+{
+    private Rigidbody2D rb;                                                 // Rigitbody2Dを格納する変数。
+
+    private static readonly float NORMAL_FALL_SPEED = 50f;                  // 通常時の落下速度（units/sec）。
+    private static readonly float FAST_FALL_SPEED = 100f;                   // 加速時の落下速度。
+    private static readonly float MOVE_DISTANCE_PER_KEY = 10f;              // 左右移動の幅。
+    private static readonly Vector3 ROTATION_ANGLE = new Vector3(0, 0, 90); // 回転角度。
+    private static readonly float GRAVITY = 30f;                            // 重力。
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.gravityScale = 0f;
+    }
+
+    void Update()
+    {
+        // 落下速度を決める処理。
+        float fallSpeed = Input.GetKey(KeyCode.DownArrow) ? FAST_FALL_SPEED : NORMAL_FALL_SPEED;
+
+        // 時間によって位置を下げる（transformを直接動かす）。
+        transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+
+        // 左に移動。
+        Move(Input.GetKeyDown(KeyCode.LeftArrow), Vector3.left);
+
+        // 右に移動。
+        Move(Input.GetKeyDown(KeyCode.RightArrow), Vector3.right);
+
+        // 回転処理。
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(ROTATION_ANGLE);
+        }
+    }
+
+    /// <summary>
+    /// 移動処理。
+    /// </summary>
+    /// <param name="getKeyDown">入力するキー。</param>
+    /// <param name="direction">移動方向。</param>
+    private void Move(bool getKeyDown, Vector3 direction)
+    {
+        if (getKeyDown)
+        {
+            transform.Translate(direction * MOVE_DISTANCE_PER_KEY, Space.World);
+        }
+    }
+
+    /// <summary>
+    /// 他のオブジェクトと当たったときの処理。
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.gravityScale = GRAVITY;
+        this.enabled = false;
+    }
+}
