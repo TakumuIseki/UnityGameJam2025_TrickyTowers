e31420caeddea1297bdf8b045a4861dc098a4d54
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-05 11:28:49 +0900

    Cartの高さを算出するスクリプトを追加。

---
+++ Assets/Game/Scripts/InGame/CartHeightCalculator.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// カートの上面の高さを算出するスクリプト。
+/// </summary>
+using UnityEngine;
+
+public class CartHeightCalculator : MonoBehaviour
+{
+    public float maxY_ { get; private set; } = 0f;    // カートの最も高い頂点のy座標を格納する。
+
+    void Start()
+    {
+        CalcCartTopY();
+    }
+
+    /// <summary>
+    /// カートの上面の高さを算出する。
+    /// </summary>
+    private void CalcCartTopY()
+    {
+        var collider = GetComponent<Collider2D>();
+        maxY_ = collider.bounds.max.y;
+    }
+}
