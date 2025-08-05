de8f77122a22cb3f6b5ad6f08bc89bd02a401c86
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-05 11:29:30 +0900

    テトリミノの高さを算出するスクリプトを追加。

---
+++ Assets/Game/Scripts/InGame/TetriminoHeightCalculator.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// テトリミノの最も高い頂点のy座標を算出するスクリプト。
+/// </summary>
+using UnityEngine;
+
+public class TetriminoHeightCalculator : MonoBehaviour
+{
+    public float maxY_ { get; private set; } = 0f;    // このテトリミノの最も高い頂点のy座標を格納する。
+
+    void Update()
+    {
+        CalcTetriminoTopY();
+    }
+
+    /// <summary>
+    /// 子である各ブロックのbounds.max.yを比較し、
+    /// 最も大きい値をこのテトリミノの頂点の高さとする。
+    /// </summary>
+    private void CalcTetriminoTopY()
+    {
+        // 初期化。
+        maxY_ = 0f;
+
+        // 子のRendererからbounds.max.yを調べる
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+        {
+            // 子のbounds.max.yをtopYに保存。
+            float topY = renderer.bounds.max.y;
+
+            // 子のbounds.max.yが記録していたyを上回ったら更新。
+            if (topY > maxY_)
+            {
+                maxY_ = topY;
+            }
+        }
+    }
+}
