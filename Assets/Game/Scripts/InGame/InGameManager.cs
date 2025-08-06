017c04ea442a2b8e736222b7fe81394f749aa318
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-06 15:49:35 +0900

    不要なusingの削除。現在制作途中のためコメントアウト。

--- Assets/Game/Scripts/InGame/InGameManager.cs
+++ Assets/Game/Scripts/InGame/InGameManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿/// <summary>
+/// インゲームの進行を管理するスクリプト。
+/// </summary>
 using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //[SerializeField] private GameObject countDown_;
+    //[SerializeField] private SpawnTetrimino spawnManager;
+    //[SerializeField] private GameTimer timer;
+
+    //[SerializeField] private float gameDuration = 60f;
+
     void Start()
     {
-        
+        // StartCoroutine(GameFlowCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private IEnumerator GameFlowCoroutine()
+    //{
+    //    countDown_.SetActive(true);
+
+    //    // 1. カウントダウン表示
+    //    yield return StartCoroutine(uiController.ShowCountdownCoroutine(3));
+
+    //    // 2. テトリミノスポナー開始（コンポーネント有効化）
+    //    spawnManager.enabled = true;
+    //    spawnManager.StartSpawning();
+
+    //    // 3. ゲームタイマー開始
+    //    timer.StartTimer(gameDuration);
+
+    //    // 4. 制限時間待機
+    //    yield return new WaitForSeconds(gameDuration);
+
+    //    // 5. ゲーム終了処理
+    //    spawnManager.StopSpawning();
+    //    spawnManager.enabled = false;
+    //    timer.StopTimer();
+
+    //    // 6. スポーン済みのテトリミノをすべて無効化
+    //    DisableAllTetriminos();
+
+    //    // 7. ゲームオーバーUI表示
+    //    uiController.ShowGameOver();
+    //}
+
+    //private void DisableAllTetriminos()
+    //{
+    //    TetriminoController[] tetriminos = FindObjectsOfType<TetriminoController>();
+    //    foreach (var tetrimino in tetriminos)
+    //    {
+    //        tetrimino.enabled = false;
+    //    }
+    //}
 }
