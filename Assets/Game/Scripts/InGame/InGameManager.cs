f842e0c68cf2b6798396e34a8d5e30d3ae06e00b
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-15 15:08:45 +0900

    プレイヤーの人数に応じて、PlayerUnitを生成する処理を追加。

--- Assets/Game/Scripts/InGame/InGameManager.cs
+++ Assets/Game/Scripts/InGame/InGameManager.cs
@@ -5,52 +5,70 @@ using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
-    //[SerializeField] private GameObject countDown_;
-    //[SerializeField] private SpawnTetrimino spawnManager;
-    //[SerializeField] private GameTimer timer;
+    [Header("PlayerUnitのプレハブ"), SerializeField]
+    private GameObject playerUnitPrefab_;                               // 複製するプレイヤーユニットのプレハブ。
 
-    //[SerializeField] private float gameDuration = 60f;
+    // コントローラーの入力に応じて変化するよう修正予定。
+    [Header("プレイヤーの人数"), SerializeField]
+    private int playerTotalCount_ = 2;
+
+    private float widthPerPlayer_ = 0f;                                 // プレイヤー1人当たりのウィンドウ幅サイズ。
+    private GameObject[] playerUnits_;                                  // プレイヤーユニットを格納。
 
     void Start()
     {
-        // StartCoroutine(GameFlowCoroutine());
+        // プレイヤー1人当たりのウィンドウ幅を設定。
+        widthPerPlayer_ = Screen.width / playerTotalCount_;
+
+        SpawnPlayerUnits(playerTotalCount_);
     }
 
-    //private IEnumerator GameFlowCoroutine()
-    //{
-    //    countDown_.SetActive(true);
+    /// <summary>
+    /// 人数分プレイヤーユニットを複製。
+    /// </summary>
+    /// <param name="count">プレイヤーの人数。</param>
+    private void SpawnPlayerUnits(int count)
+    {
+        // playerUnit_の空オブジェクトを生成。
+        playerUnits_ = new GameObject[count];
 
-    //    // 1. カウントダウン表示
-    //    yield return StartCoroutine(uiController.ShowCountdownCoroutine(3));
+        // プレイヤーの人数分処理。
+        for (int num = 0; num < count; num++)
+        {
+            // プレイヤーユニットを生成。
+            GameObject unit = Instantiate(playerUnitPrefab_);
 
-    //    // 2. テトリミノスポナー開始（コンポーネント有効化）
-    //    spawnManager.enabled = true;
-    //    spawnManager.StartSpawning();
+            // 名前に番号を付与。
+            unit.name = "PlayerUnit" + (num + 1);
 
-    //    // 3. ゲームタイマー開始
-    //    timer.StartTimer(gameDuration);
+            // 位置を設定。
+            unit.transform.position = new Vector2(GetFirstPlayerWindowCenterX() + num * widthPerPlayer_, GetWindowCenterY());
 
-    //    // 4. 制限時間待機
-    //    yield return new WaitForSeconds(gameDuration);
+            // PlayerUnitとその子にPlayerTypeをセットする。
+            var id = unit.GetComponentsInChildren<PlayerTypeComponent>();
+            foreach (var comp in id)
+            {
+                comp.playerType = num + 1;
+            }
 
-    //    // 5. ゲーム終了処理
-    //    spawnManager.StopSpawning();
-    //    spawnManager.enabled = false;
-    //    timer.StopTimer();
+            // playerUnits_に格納する。
+            playerUnits_[num] = unit;
+        }
+    }
 
-    //    // 6. スポーン済みのテトリミノをすべて無効化
-    //    DisableAllTetriminos();
+    /// <summary>
+    /// 1人目のプレイヤーのウィンドウの中央位置（X座標）を取得。
+    /// </summary>
+    /// <returns></returns>
+    private float GetFirstPlayerWindowCenterX()
+    {
+        float firstPlayerWindowCenterX = Screen.width / playerTotalCount_ - widthPerPlayer_ / 2;
+        return firstPlayerWindowCenterX;
+    }
 
-    //    // 7. ゲームオーバーUI表示
-    //    uiController.ShowGameOver();
-    //}
-
-    //private void DisableAllTetriminos()
-    //{
-    //    TetriminoController[] tetriminos = FindObjectsOfType<TetriminoController>();
-    //    foreach (var tetrimino in tetriminos)
-    //    {
-    //        tetrimino.enabled = false;
-    //    }
-    //}
+    private float GetWindowCenterY()
+    {
+        float windowCenterY = Screen.height / 2;
+        return windowCenterY;
+    }
 }
