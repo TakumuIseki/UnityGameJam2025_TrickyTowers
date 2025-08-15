d51414082b807a364a53fafe69e8ebaeab51add5
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-15 15:06:52 +0900

    名前を変更。自分が属するプレイヤーユニットと同じプレイヤーユニットに属するカートを取得するコードを追加。Towerタグを持つテトリミノが増減したときだけFindをかけるよう修正。

--- Assets/Game/Scripts/InGame/TetriminoControllerComponent.cs
+++ Assets/Game/Scripts/InGame/TetriminoControllerComponent.cs
@@ -3,7 +3,7 @@
 /// </summary>
 using UnityEngine;
 
-public class ControllableBlock : MonoBehaviour
+public class TetriminoControllerComponent : MonoBehaviour
 {
     private static readonly float NORMAL_FALL_SPEED = 50f;                  // 通常時の落下速度（units/sec）。
     private static readonly float FAST_FALL_SPEED = 100f;                   // 加速時の落下速度。
@@ -12,30 +12,40 @@ public class ControllableBlock : MonoBehaviour
     private static readonly float GRAVITY = 30f;                            // 重力。
     private static readonly float DESTROY_Y_THRESHOLD = -40f;               // このY座標を下回ったら消滅。
 
-    private Rigidbody2D rb_;                                                 // Rigitbody2Dを格納する変数。
-    private SpawnTetrimino spawner_;                                         // SpawnTetrimino型の変数。
-    private bool hasCollided_ = false;                                       // 当たり判定が一度検出されたら立てるフラグ。
+    [Header("自身のRigidBody2D"), SerializeField]
+    private Rigidbody2D rigidBody_;
 
     [Header("付与するタグ"), SerializeField]
     private string tagToAssign_ = "Tower";
 
+    private TowerHeightCalculatorComponent towerHeightCalculator_;          // TowerHeightCalculatorComponent型の変数。
+    private TetriminoSpawnerComponent spawner_;                             // SpawnTetrimino型の変数。
+    private bool hasCollided_ = false;                                      // 当たり判定が一度検出されたら立てるフラグ。
+
     void Start()
     {
-        rb_ = GetComponent<Rigidbody2D>();
-        rb_.bodyType = RigidbodyType2D.Dynamic;
-        rb_.gravityScale = 0f;
+        FindAndAssignTowerHeightCalculator();
     }
-
     void Update()
     {
-        DestroyIfBelowThreshold();
-
         // 当たり判定を検出したら、操作不可にする。
-        if (hasCollided_)
+        if (!hasCollided_)
         {
-            return;
+            HandleMovementAndRotation();
         }
 
+        // テトリミノのy座標が一定以下になったら、削除する。
+        if (transform.position.y < DESTROY_Y_THRESHOLD)
+        {
+            DestroyIfBelowThreshold();
+        }
+    }
+
+    /// <summary>
+    /// テトリミノの移動と回転処理。
+    /// </summary>
+    private void HandleMovementAndRotation()
+    {
         // 落下速度を決める処理。
         float fallSpeed = Input.GetKey(KeyCode.DownArrow) ? FAST_FALL_SPEED : NORMAL_FALL_SPEED;
 
@@ -56,7 +66,7 @@ public class ControllableBlock : MonoBehaviour
     }
 
     /// <summary>
-    /// 移動処理。
+    /// 左右移動処理用メソッド。
     /// </summary>
     /// <param name="getKeyDown">入力するキー。</param>
     /// <param name="direction">移動方向。</param>
@@ -71,9 +81,31 @@ public class ControllableBlock : MonoBehaviour
     /// <summary>
     /// テトリミノを生成するスポナーを設定する。
     /// </summary>
-    public void SetSpawner(SpawnTetrimino spawner)
+    public void SetSpawner(TetriminoSpawnerComponent spawner)
     {
-        this.spawner_ = spawner;
+        spawner_ = spawner;
+    }
+
+    /// <summary>
+    /// 同じPlayerUnitに属するHeightを取得。
+    /// </summary>
+    private void FindAndAssignTowerHeightCalculator()
+    {
+        // 自分が属しているPlayerUnitを取得。
+        // (テトリミノ→スポナー→プレイヤーユニット)
+        var playerUnit = transform.parent.parent;
+
+        // 自分が属しているPlayerUnitの中から、
+        // TowerHeightCalculatorComponentを取得してセット。
+        towerHeightCalculator_ = playerUnit.GetComponentInChildren<TowerHeightCalculatorComponent>();
+
+        // towerHeightCalculator_が見つからない場合は警告。
+        if (towerHeightCalculator_ == null)
+        {
+            Debug.LogError($"towerHeightCalculator_が見つかりません。" +
+                $"PlayerUnitとの親子関係やTowerHeightCalculatorComponentが" +
+                $"PlayerUnit内でアタッチされているかを確認してください。");
+        }
     }
 
     /// <summary>
@@ -89,13 +121,16 @@ public class ControllableBlock : MonoBehaviour
         hasCollided_ = true;
 
         // 重力を設定する。
-        rb_.gravityScale = GRAVITY;
+        rigidBody_.gravityScale = GRAVITY;
 
         // 次のテトリミノをスポーンさせる。
         spawner_.Spawn();
 
         // タグを付与する。
         gameObject.tag = tagToAssign_;
+
+        // Towerタグを持ったテトリミノのリストを更新。
+        towerHeightCalculator_.FindTetriminosWithTagTower();
     }
 
     /// <summary>
@@ -103,18 +138,19 @@ public class ControllableBlock : MonoBehaviour
     /// </summary>
     private void DestroyIfBelowThreshold()
     {
-        // テトリミノのy座標が一定以下になったら。
-        if (transform.position.y < DESTROY_Y_THRESHOLD)
-        {
-            // テトリミノ自身を削除する。
-            Destroy(gameObject);
+        // テトリミノ自身を削除する。
+        Destroy(gameObject);
 
-            // テトリミノにtowerタグが付いていなかったら。
-            if (gameObject.tag != tagToAssign_)
-            {
-                // 次のテトリミノをスポーンさせる。
-                spawner_.Spawn();
-            }
+        // テトリミノにtowerタグが付いていたら。
+        if (gameObject.tag == tagToAssign_)
+        {
+            // Heightが持つ、Towerタグを持ったテトリミノのリストを更新。
+            towerHeightCalculator_.FindTetriminosWithTagTower();
+        }
+        else
+        {
+            // 次のテトリミノをスポーンさせる。
+            spawner_.Spawn();
         }
     }
 }
