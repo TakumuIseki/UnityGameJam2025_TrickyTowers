acc6c790fe63b46122d6c530351f5dd091e90d13
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-08-15 15:03:43 +0900

    名前を変更しました。また、カート出現後の座標が取得できていなかったため、1フレーム待機するよう修正しました。

---
+++ Assets/Game/Scripts/InGame/CartHeightCalculatorComponent.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// カートの上面の高さを算出するスクリプト。
+/// </summary>
+using UnityEngine;
+
+public class CartHeightCalculatorComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D colli;
+    public float TopY { get; private set; } = 0f;    // カートの最も高い頂点のy座標を格納する。
+
+    private void Start()
+    {
+        StartCoroutine(InitAfterPhysics());
+    }
+
+    /// <summary>
+    /// 現在のフレームの物理演算が完了した後、オブジェクトを初期化します。
+    /// </summary>
+    private System.Collections.IEnumerator InitAfterPhysics()
+    {
+        // 1フレーム待って物理計算後に取得
+        yield return new WaitForFixedUpdate();
+        TopY = colli.bounds.max.y;
+    }
+}
